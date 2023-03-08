f763e06215485f53e1dcf352e7e7900cfb6a91fc
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-03-07 23:15:33 -0300

    Criação Components Header e Tag

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import { RiShutDownLine} from 'react-icons/ri';
+import { Container, Profile, Logout } from "./styles";
+
+export function Header() {
+    return (
+        <Container>
+            <Profile>
+                <img
+                    src="https://github.com/gisellebm.png"
+                    alt="Foto do Usuário"
+                />
+
+                <div>
+                    <span>Bem vindo,</span>
+                    <strong>Giselle Brasil</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
+        </Container>
+    )
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,16 +1,28 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
+import { Header } from "../../components/Header";
 import { Button } from '../../components/Button';
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
 
   return(
     <Container>
-      <h1>Hello Word!</h1>
-      <span>Giselle Brasil</span>
+      <Header />
 
-      <Button title="Entrar" loading />
-      <Button title="Cadastrar" />
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
+
       <Button title="Voltar" />
     </Container>
   )
