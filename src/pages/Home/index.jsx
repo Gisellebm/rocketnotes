fff614252eac53e7960531e00ceae177d7a551b6
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-03-09 23:26:40 -0300

    Finalização da interface Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,10 @@
+import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+import { Note } from '../../components/Note'
+import { Input } from '../../components/input';
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
     return (
@@ -10,13 +15,32 @@ export function Home() {
 
             <Header></Header>
 
-            <Menu></Menu>
+            <Menu>
+                <li><ButtonText title="Todos" isActive /></li>
+                <li><ButtonText title="React" /></li>
+                <li><ButtonText title="Nodejs" /></li>
+            </Menu>
 
-            <Search></Search>
+            <Search>
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+            </Search>
 
-            <Content></Content>
+            <Content>
+                <Section title="Minhas Notas">
+                    <Note data={{
+                        title: 'React',
+                        tags: [
+                            { id: '1', name: 'react' },
+                            { id: '2', name: 'rocketseat' }
+                        ]
+                    }} />
+                </Section>
+            </Content>
 
-            <NewNote></NewNote>
+            <NewNote>
+                <FiPlus />
+                Criar Nota
+            </NewNote>
         </Container>
     )
 }
