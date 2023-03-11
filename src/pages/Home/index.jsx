a63be66cf39b125f15a063bc81f754c9200d7cfb
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-03-10 21:47:39 -0300

    "Finalização da interface Profile"

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import { FiPlus, FiSearch } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 import { Note } from '../../components/Note'
-import { Input } from '../../components/input';
+import { Input } from '../../components/Input';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,7 @@
 import { Container, Form, Background } from './styles';
 import { FiMail, FiLock } from 'react-icons/fi';
 
-import { Input } from '../../components/input';
+import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 
 export function SignIn() {
